c7fb8b2616402d43f9896a46a093436e36a4a9d7
saiashish9 <saiashish3760@gmail.com>
2021-01-03 04:41:24 +0530

    mainactivity.java and layouts updated

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.new_layout);
+
+        btn = (Button) findViewById(R.id.button2);
+        btn.setText("hi 123");
+
     }
 }
