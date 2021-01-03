9a37bb167bc3200d66426fa42d1be445a0455c3c
saiashish9 <saiashish3760@gmail.com>
2021-01-03 19:30:20 +0530

    RadioButton & RadioGroup completed

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.test;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,11 +11,36 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
 
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        radioGroup = (RadioGroup) findViewById(R.id.rbg);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                radioButton = (RadioButton) findViewById(checkedId);
+
+                switch (radioButton.getId()) {
+                    case R.id.rb1: {
+                        if (radioButton.isChecked()) {
+                            Log.d("RD", "yes !!");
+                        }
+                    }
+                    break;
+                    case R.id.rb2: {
+                        if (radioButton.isChecked()) {
+                            Log.d("RD", "no !!");
+                        }
+                    }
+                }
+
+            }
+        });
 
     }
 
