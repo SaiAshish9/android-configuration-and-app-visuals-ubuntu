320aa012b17ae9c1a5548bdf90c8d0da8ef06a57
saiashish9 <saiashish3760@gmail.com>
2021-01-03 14:15:42 +0530

    onclick listeners  updated

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,26 +2,46 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private Button btn;
+//    private Button btn;
 
+    private Button mButton;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.new_layout);
+        setContentView(R.layout.activity_main);
+        mButton = (Button) findViewById(R.id.mButton);
+        mTextView = (TextView) findViewById(R.id.mTextView);
+        mButton.setText(R.string.button_name);
+//        btn = (Button) findViewById(R.id.button2);
+//        btn.setText("hi 123");
+//        btn.setBackgroundColor(Color.BLACK);
 
-        btn = (Button) findViewById(R.id.button2);
-        btn.setText("hi 123");
-        btn.setBackgroundColor(Color.BLACK);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTextView.setVisibility(View.VISIBLE);
+                mTextView.setText(R.string.show_text);
+            }
+        });
 
     }
+
+
+//    public void showMe(View view){
+//        mTextView.setVisibility(View.VISIBLE);
+//        mTextView.setText(R.string.show_text);
+//    }
+
+
 }
